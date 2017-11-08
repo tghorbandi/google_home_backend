365a21abcbd3452a9f6a329f1d0e4bde217a1b67
Tawab Ghorbandi <tawab@e-sites.nl>
2017-11-08 16:11:02 +0100

    Welcome req 1

--- routes/testPost.js
+++ routes/testPost.js
@@ -3,79 +3,98 @@ var router = express.Router();
 var mongoDBqueries = require('../controllers/mongoDB');
 var socket;
 
+// Set Variables
+var intro = 1;
 var inhoud = null;
 
   router.post('/', function(req, res) {
 
-    if (!req.body) {
+        if (!req.body) {
 
-    	return res.sendStatus(400);
+            return res.sendStatus(400);
 
-    } else{
+        } else{
 
-      // Query in DB zetten
-      var productName = req.body.result.resolvedQuery;
-      console.log("JSON resolvedQuery: " + productName);
+        // Save Query in variable
+        var productName = req.body.result.resolvedQuery;
+        console.log("JSON resolvedQuery: " + productName);
 
-      // Opslaan in database
-      //mongoDBqueries.insertQuery(function(result){}, productName);
+        console.log(req.body.result);
 
-      //Get context parameter van json request
-      var productType =  req.body.result.contexts[0].parameters;
-      var productType2 = productType[Object.keys(productType)[0]];
-
-
-      if(req.body.result.contexts[0].name === "sledge_hammer"){
-          productType2 = "sledge hammer";
-          socket.emit('productName', { productName: productType2});
-      }
-
-      console.log("productType2: " + productType2);
-
-      // //context overeenkomt met product in database
-      mongoDBqueries.findProduct(function(result){
-            //console.log(result);
-            inhoud = result;
-        }, productType2);
+        // check first time for a welcome intent
+        // after the first time, give other welcome responses
+        if (intro === 1){
+            var welcome = req.body.result.action;
+            if (welcome === "input.welcome"){
+                intro = 2;
+                return res.json({
+                    speech: "Hi! I'm e-sites digital assistant. I am designed to give advice and help about Do-it-Yourself store products. How can I help you?"
+                });
+            }
+        }else{
+            return res.json({
+                speech: "Hi! I'm e-sites digital assistant. How can I help?"
+            });
+        }
 
 
-      socket.emit('productName', { productName: productType2});
+        // Save query variable in mongoDB
+        //mongoDBqueries.insertQuery(function(result){}, productName);
+
+        //Get context parameter van json request
+        var productType =  req.body.result.contexts[0].parameters;
+        var productType2 = productType[Object.keys(productType)[0]];
 
 
-      // Als er inderdaad een overeenkomst is in de database
-      if(inhoud === true){
-        console.log('inhoud!');
-        // product / context doorsturen naar client
+        if(req.body.result.contexts[0].name === "sledge_hammer"){
+            productType2 = "sledge hammer";
+            socket.emit('productName', { productName: productType2});
+        }
+
+        console.log("productType2: " + productType2);
+
+        // //context overeenkomt met product in database
+        mongoDBqueries.findProduct(function(result){
+              //console.log(result);
+              inhoud = result;
+          }, productType2);
+
+
         socket.emit('productName', { productName: productType2});
-      }else{
-        // Als er geen product overeenkomsten zijn met database
-
-        //socket.emit('noProduct', { data: "No product found"});
-
-          // return res.json({
-          //     speech: "Default webhook response"
-          // });
-      }
 
 
-      // if(req.body.result.resolvedQuery.indexOf('find')){
-      //   return res.json({
-      //           speech: "What product are you looking for?",
-      //   });
-      // }
-
-      // return res.json({
-      //     speech: "testing from webhook",
-      //     followupEvent: {
-      //       "name": "fill_slots",
-      //       "data": {
-      //         "food": "chicken",
-      //         "amount": "200"
-      //       },
-      //     }
-      // });
+        // Als er inderdaad een overeenkomst is in de database
+        if(inhoud === true){
+            console.log('inhoud!');
+            // product / context doorsturen naar client
+            socket.emit('productName', { productName: productType2});
+        }else{
+            // Als er geen product overeenkomsten zijn met database
+            //socket.emit('noProduct', { data: "No product found"});
 
 
+            // return res.json({
+            //     speech: "Default webhook response"
+            // });
+        }
+
+
+        // if(req.body.result.resolvedQuery.indexOf('find')){
+        //   return res.json({
+        //           speech: "What product are you looking for?",
+        //   });
+        // }
+
+        // return res.json({
+        //     speech: "testing from webhook",
+        //     followupEvent: {
+        //       "name": "fill_slots",
+        //       "data": {
+        //         "food": "chicken",
+        //         "amount": "200"
+        //       },
+        //     }
+        // });
 
   }
 
