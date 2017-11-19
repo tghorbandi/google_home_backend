a0e275b5c31ea501a26aad108c98480c03103725
TawabG <tghorbandi@gmail.com>
2017-11-19 22:03:38 +0100

    inputArray

--- routes/products.js
+++ routes/products.js
@@ -90,11 +90,7 @@ router.post('/', function(req, res) {
             mongoDBqueries.findSpecificType(function(result){
                   console.log("MONGODB RESULT:" + JSON.stringify(result));
 
-                  console.log(result.type);
-                  console.log(result[0]);
-                  var inhoud = JSON.stringify(result);
-                  var inhoud2 = JSON.parse(inhoud);
-                  console.log(inhoud2.type);
+                  console.log(result[0].type);
 
 
                   if(JSON.stringify(result).length > 0){
