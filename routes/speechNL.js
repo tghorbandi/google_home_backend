35ed662eebb8bb622da1019f940371f5a9c4980f
Tawab Ghorbandi <tawab@e-sites.nl>
2017-11-24 14:17:27 +0100

    dutch version

--- routes/speechNL.js
+++ routes/speechNL.js
@@ -12,15 +12,21 @@ router.post('/', function(req, res) {
 
     } else {
 
+        /*
+        * Welcome intent
+        * */
         if (req.body.result.metadata.intentId === "ce927094-960f-4f0d-8dd0-3b3e2ae65658") {
 
-            socket.emit('speech', { text: "Hallo, ik ben ejsites, Waarmee kan ik je helpen?"});
+            socket.emit('speech', { text: "Hallo, Waarmee kan ik je helpen?"});
 
             return res.json({
                 speech: " "
             });
         }
 
+        /*
+        * Hamers
+        * */
         if (req.body.result.metadata.intentId === "e7cbd274-f73f-4f58-937c-165bb2b8708b") {
 
             socket.emit('speech', { text: "De hamers liggen in gangpad B. rij 3"});
@@ -34,6 +40,20 @@ router.post('/', function(req, res) {
 
         }
 
+        /*
+        * Verf
+        * */
+        if (req.body.result.metadata.intentId === "4c2145e6-bf2c-429c-b7f8-672b928d52c8") {
+
+            socket.emit('speech', { text: "De verf assortiment kun je vinden in gangpad D"});
+
+            socket.emit('text1', { text1: "Gangpad D"});
+
+            return res.json({
+                speech: " "
+            });
+
+        }
 
 
         return res.sendStatus(200);
