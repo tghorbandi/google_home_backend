4741d11c4b094796b20eed3d5ddd45b06af94853
Tawab Ghorbandi <tawab@e-sites.nl>
2017-11-24 11:50:41 +0100

    dutch version

--- routes/speechNL.js
+++ routes/speechNL.js
@@ -14,7 +14,8 @@ router.post('/', function(req, res) {
 
         if (req.body.result.metadata.intentId === "3f0a2917-4725-4aaf-9970-1f9f13852cba") {
 
-            socket.emit('speech', { text: "De hamers liggen in gangpad B. Sectie 3"});
+            //socket.emit('speech', { text: "De hamers liggen in gangpad B. Sectie 3"});
+            socket.emit('speech', { text: "Hallo, ik ben ejsites, Waarmee kan ik je helpen?"});
 
             console.log('testtt');
             return res.json({
