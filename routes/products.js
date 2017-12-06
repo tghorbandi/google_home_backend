49dee581b1fc97603181977714ead2cf748ad406
Tawab Ghorbandi <tawab@e-sites.nl>
2017-12-06 15:25:24 +0100

    added regular hammer webpage

--- routes/products.js
+++ routes/products.js
@@ -212,6 +212,7 @@ router.post('/', function(req, res) {
          */
         if (req.body.result.metadata.intentId === "00768954-4b2e-4e79-8f79-f20d5fda1818" || req.body.result.metadata.intentId === "55c24519-5439-4cbe-a0c0-2159d5c71e4e"){
             mongoDBqueries.findProductWithIntentId(function(result) {
+                console.log("MONGODB RESULT:" + JSON.stringify(result));
                 if(result){
                     socket.emit('productDetails', {
                         productName: result[0].fullProductName,
