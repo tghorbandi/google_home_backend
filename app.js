2ddba1c732445a442e694f4b7f1e81a0794ba399
Tawab Ghorbandi <tawab@e-sites.nl>
2018-01-12 11:36:44 +0100

    Added floorplan

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var mysql = require('mysql');
 var socket_io  = require('socket.io');
+var favicon = require('serve-favicon');
 
 // ***************
 // USE ROUTES NEW
@@ -57,7 +58,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(favicon(path.join(__dirname,'public','images','favicon.ico')));
 //app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
